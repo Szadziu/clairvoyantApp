96cea90087e7e6e8944062696b0ffdc6e3ffc665
Szadziu <mcj.mikolajczak@gmail.com>
2021-03-27 21:31:57 +0100

    my first post on GH

---
+++ main.js
@@ -0,0 +1,35 @@
+const addButton = document.querySelector(".add");
+const resetButton = document.querySelector(".clean");
+const showAdvice = document.querySelector(".showAdvice");
+const showOptions = document.querySelector(".showOptions");
+const input = document.querySelector("input");
+const screenResult = document.querySelector("h1");
+
+const arrayOptions = [];
+
+const addOptions = (e) => {
+  e.preventDefault();
+  let userValue = input.value;
+  arrayOptions.push(userValue);
+  input.value = "";
+};
+
+const resetArray = (e) => {
+  e.preventDefault();
+  arrayOptions.length = 0;
+};
+
+const showHint = () => {
+  const rdmIndex = Math.floor(Math.random() * arrayOptions.length);
+  screenResult.textContent = arrayOptions[rdmIndex];
+};
+
+const showPossibilities = () => {
+  newArray = arrayOptions.join(" --- ");
+  screenResult.textContent = newArray;
+};
+
+addButton.addEventListener("click", addOptions);
+resetButton.addEventListener("click", resetArray);
+showAdvice.addEventListener("click", showHint);
+showOptions.addEventListener("click", showPossibilities);
